ffaba25a1d1554a1cedfdc131a9adbe4d5c03eb2
bayurizki06 <84717250+bayurizki06@users.noreply.github.com>
2023-10-10 01:01:49 +0700

    Update hasilbooking.js merapihkan variable

--- js/hasilbooking.js
+++ js/hasilbooking.js
@@ -3,17 +3,17 @@ const hasilBooking = document.getElementById("hasilBooking");
 const apiUrl = 'https://be-semarang-28-production.up.railway.app/bookings';
 
 fetch(apiUrl)
-.then(response => {
-    if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-})
-.then(data => {
-       console.log(data[data.length-1]);
-    hasilBooking.textContent = 
-        data[data.length - 1].bookingID + " " + data[data.length - 1].patientname  + data[data.length - 1].doctor + " " + data[data.length - 1].clinic  + " " + data[data.length - 1].appointment_time ;
-})
-.catch(error => {
-    console.error('Fetch error:', error);
-});
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        const lastBooking = data[data.length - 1];
+        const bookingDetails = `${lastBooking.bookingID} ${lastBooking.patientname} ${lastBooking.doctor} ${lastBooking.clinic} ${lastBooking.appointment_time}`;
+        hasilBooking.textContent = bookingDetails;
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
