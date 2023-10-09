6f9006423582318903bd8821452e69546a5d1ae2
bayurizki06 <84717250+bayurizki06@users.noreply.github.com>
2023-10-09 22:35:40 +0700

    ini logic page hasil booking

---
+++ js/hasilbooking.js
@@ -0,0 +1,22 @@
+const hasilBooking = document.getElementById("hasilBooking");
+
+// Define the API URL you want to fetch data from
+const apiUrl = 'http://be-semarang-28-production.up.railway.app/bookings';
+
+// Use the fetch() function to make a GET request
+fetch(apiUrl)
+.then(response => {
+    // Check if the response status is OK (200)
+    if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    // Parse the JSON response
+    return response.json();
+})
+.then(data => {
+    // Work with the data
+    console.log(data);
+})
+.catch(error => {
+    console.error('Fetch error:', error);
+});
