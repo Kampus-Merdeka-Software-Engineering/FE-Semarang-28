2e8343f2cc22fad1a94b254295ee09156067ba6a
bayurizki06 <84717250+bayurizki06@users.noreply.github.com>
2023-10-09 23:26:42 +0700

    Update hasilbooking.js sesuai struktur data di db

--- js/hasilbooking.js
+++ js/hasilbooking.js
@@ -1,22 +1,18 @@
 const hasilBooking = document.getElementById("hasilBooking");
 
-// Define the API URL you want to fetch data from
 const apiUrl = 'https://be-semarang-28-production.up.railway.app/bookings';
 
-// Use the fetch() function to make a GET request
 fetch(apiUrl)
 .then(response => {
-    // Check if the response status is OK (200)
     if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    // Parse the JSON response
     return response.json();
 })
 .then(data => {
-    // Work with the data
        console.log(data[data.length-1]);
-    hasilBooking.textContent = data[data.length - 1].bookingID + " " + data[data.length - 1].patientname;
+    hasilBooking.textContent = 
+        data[data.length - 1].bookingID + " " + data[data.length - 1].patientname  + data[data.length - 1].doctor + " " + data[data.length - 1].clinic  + " " + data[data.length - 1].appointment_time ;
 })
 .catch(error => {
     console.error('Fetch error:', error);
