1e314fd72e51a5ac233334e734d2a33eb9fe4f56
bayurizki06 <84717250+bayurizki06@users.noreply.github.com>
2023-10-10 00:54:22 +0700

    Update airquality.js

--- js/airquality.js
+++ js/airquality.js
@@ -1,6 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-    const apiKey = '4c7fce2d-a3b5-4500-8b95-58a1232425fb'; // Air Visual API key Bayu
+    const apiKey = '4c7fce2d-a3b5-4500-8b95-58a1232425fb'; 
     const apiUrl = `https://api.airvisual.com/v2/nearest_city?key=${apiKey}`;
 
     fetch(apiUrl)
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const temperatureElement = document.getElementById('temperature');
             const humidityElement = document.getElementById('humidity');
 
-            // Update HTML oakai data
+            // Update HTML pakai
             cityNameElement.textContent = data.data.city;
             aqiElement.textContent = data.data.current.pollution.aqius;
             temperatureElement.textContent = data.data.current.weather.tp;
